f88cc4504e3fdc7db5a4cce9dfde8cfbe894596c
Matt Ridley <mridley@gmail.com>
2016-02-26 21:12:45 -0600

    Initial Project Creation

---
+++ camper/camper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  camper
+//
+//  Created by Matthew Ridley on 2/26/16.
+//  Copyright © 2016 That Conference. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
