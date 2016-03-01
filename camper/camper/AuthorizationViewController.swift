73870f1fcbed724f259689382d5667f8f94cf9a8
Matt Ridley <mridley@gmail.com>
2016-02-29 22:28:51 -0600

    Changed start to open tab and fixed tab coloring

--- camper/camper/AuthorizationViewController.swift
+++ camper/camper/AuthorizationViewController.swift
@@ -2,4 +2,7 @@ import UIKit
 
 class AuthorizationViewController : UIViewController {
     
+    @IBAction func authorizeUsernamePassword(sender: AnyObject) {
+        //TODO: Authorize using username/password
+    }
 }
