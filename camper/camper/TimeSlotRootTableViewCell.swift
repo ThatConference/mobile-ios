1e7a91d14037583ba2eba6548d67c5c53a2001be
Steven Yang <stevenyang@Stevens-MBP.attlocal.net>
2017-04-12 16:17:37 -0500

    Completed swipe gesture

--- camper/camper/TimeSlotRootTableViewCell.swift
+++ camper/camper/TimeSlotRootTableViewCell.swift
@@ -2,4 +2,23 @@ import UIKit
 
 class TimeSlotRootTableViewCell: UITableViewCell {
     var session: Session!
+    var delegate: ScheduleCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(ScheduleTableViewCell.swipeCellFunc(sender:)))
+        swipeGesture.direction = .left
+        addGestureRecognizer(swipeGesture)
+    }
+    
+    func swipeCellFunc(sender: UISwipeGestureRecognizer) {
+        if sender.direction == .left {
+            delegate?.ScheduleCellDelegate(session)
+        }
+    }
+}
+
+protocol ScheduleCellDelegate {
+    func ScheduleCellDelegate(_ session: Session)
 }
